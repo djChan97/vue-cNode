a543d9891bd7400ffc638cb99e8ddcd0972272c0
XXIANG1997 <moldysaltedfish@gmail.com>
2019-12-28 20:33:52 +0800

    modify deploy file

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:XXIANG1997/Vue-CNode.git master:gh-pages
+git push -f git@github.com:XXIANG1997/CNode-Vue.git master:gh-pages
 
 cd -
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? '/Vue-CNode/' : '/'
+	publicPath: process.env.NODE_ENV === 'production' ? '/CNode-Vue/' : '/'
 }
